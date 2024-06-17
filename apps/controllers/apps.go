67c0caf01c7d746c7e2324eec55c6ddbc5ee3cff
Joao Ribeiro da Silva <joao.r.silva@gmail.com>
2024-06-17 13:49:49 -0300

    fix git

--- apps/controllers/apps.go
+++ apps/controllers/apps.go
@@ -4,134 +4,155 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joaoribeirodasilva/teos/common/models"
-	"github.com/joaoribeirodasilva/teos/common/responses"
-	"github.com/joaoribeirodasilva/teos/common/service_log"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 func AppAppsList(c *gin.Context) {
 
-	m := models.NewAppAppsModel(c)
-	docs := []models.AppAppModel{}
+	//vars := controllers.GetValues(c)
 
-	count, appErr := m.FindAllBuQueryFilter(&docs)
-	if appErr != nil {
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-	}
+	/* 	model := models.AppAppModel{}
+	   	models := []models.AppAppModel{}
 
-	m.Count = count
-	m.Rows = docs
+	   	coll := models.NewBaseCollection(
+	   		model.GetCollectionName(),
+	   		vars.Db,
+	   		nil,
+	   	)
 
-	c.JSON(http.StatusOK, &m)
+	   	logger.Info("will insert test on the database")
+
+	   	userId := primitive.NewObjectID()
+	   	model.SetUserID(&vars.UserID)
+
+	   	count, err := coll.FindAll(vars.Query.filter, &models)
+	   	if err != nil {
+	   		if err != models.ErrNotFound {
+	   			httpErr := logger.Error(logger.LogStatusNotFound, nil, "", err, nil)
+	   			c.AbortWithStatusJSON(httpErr.Status, httpErr)
+	   			return
+	   		}
+	   		httpErr := logger.Error(logger.LogStatusNotFound, nil, "", err, nil)
+	   		c.AbortWithStatusJSON(httpErr.Status, httpErr)
+	   		return
+	   	}
+
+	   	m.Count = count
+	   	m.Rows = docs */
+
+	//c.JSON(http.StatusOK, &m)
+	c.Status(http.StatusOK)
 }
 
 func AppAppsGet(c *gin.Context) {
 
-	doc := models.NewAppAppModel(c)
+	/*
+		 	doc := models.NewAppAppModel(c)
 
-	if appErr := doc.FindByQueryID(doc); appErr != nil {
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-		return
-	}
+			if appErr := doc.FindByQueryID(doc); appErr != nil {
+				c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+				return
+			}
 
-	c.JSON(http.StatusOK, &doc)
+			c.JSON(http.StatusOK, &doc)
+	*/
+	c.Status(http.StatusOK)
 }
 
 func AppAppsCreate(c *gin.Context) {
 
-	doc := models.NewAppAppModel(c)
+	/* 	doc := models.NewAppAppModel(c)
 
-	if appErr := doc.Bind(); appErr != nil {
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-		return
-	}
+	   	if appErr := doc.Bind(); appErr != nil {
+	   		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+	   		return
+	   	}
 
-	if appErr := doc.Validate(); appErr != nil {
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-		return
-	}
+	   	if appErr := doc.Validate(); appErr != nil {
+	   		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+	   		return
+	   	}
 
-	doc.FillMeta(true, false)
+	   	doc.FillMeta(true, false)
 
-	if appErr := doc.Create(bson.D{{Key: "appKey", Value: doc.AppKey}}, doc, "appKey"); appErr != nil {
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-		return
-	}
+	   	if appErr := doc.Create(bson.D{{Key: "appKey", Value: doc.AppKey}}, doc, "appKey"); appErr != nil {
+	   		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+	   		return
+	   	}
 
-	response := responses.ResponseCreated{
-		ID: doc.ID,
-	}
-	c.JSON(http.StatusCreated, response)
+	   	response := responses.ResponseCreated{
+	   		ID: doc.ID,
+	   	}
+	   	c.JSON(http.StatusCreated, response) */
+	c.Status(http.StatusOK)
 }
 
 func AppAppsUpdate(c *gin.Context) {
 
-	doc := models.NewAppAppModel(c)
+	/* 	doc := models.NewAppAppModel(c)
 
-	if appErr := doc.Bind(); appErr != nil {
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-		return
-	}
+	   	if appErr := doc.Bind(); appErr != nil {
+	   		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+	   		return
+	   	}
 
-	if appErr := doc.Validate(); appErr != nil {
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-		return
-	}
+	   	if appErr := doc.Validate(); appErr != nil {
+	   		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+	   		return
+	   	}
 
-	id := doc.GetValues().Query.ID
-	if id == nil {
-		appErr := service_log.Error(0, http.StatusBadRequest, "CONTROLLER::AppApps::Update:", "", "invalid id")
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-		return
-	}
+	   	id := doc.GetValues().Query.ID
+	   	if id == nil {
+	   		appErr := service_log.Error(0, http.StatusBadRequest, "CONTROLLER::AppApps::Update:", "", "invalid id")
+	   		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+	   		return
+	   	}
 
-	doc.ID = *id
+	   	doc.ID = *id
 
-	exists := models.NewAppAppModel(c)
-	if appErr := exists.First(bson.D{{Key: "appKey", Value: doc.AppKey}}, exists); appErr != nil {
-		if appErr.HttpCode != http.StatusNotFound {
-			c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-			return
-		}
-	}
+	   	exists := models.NewAppAppModel(c)
+	   	if appErr := exists.First(bson.D{{Key: "appKey", Value: doc.AppKey}}, exists); appErr != nil {
+	   		if appErr.HttpCode != http.StatusNotFound {
+	   			c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+	   			return
+	   		}
+	   	}
 
-	if exists.ID != doc.ID {
-		appErr := service_log.Error(0, http.StatusConflict, "[CONTROLLERS]::AppApp::Update", "appKey", "document already exists with id: %s", exists.ID.Hex())
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-		return
-	}
+	   	if exists.ID != doc.ID {
+	   		appErr := service_log.Error(0, http.StatusConflict, "[CONTROLLERS]::AppApp::Update", "appKey", "document already exists with id: %s", exists.ID.Hex())
+	   		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+	   		return
+	   	}
 
-	exists.Name = doc.Name
-	exists.Description = doc.Description
-	exists.AppKey = doc.AppKey
-	exists.Active = doc.Active
+	   	exists.Name = doc.Name
+	   	exists.Description = doc.Description
+	   	exists.AppKey = doc.AppKey
+	   	exists.Active = doc.Active
 
-	doc.FillMeta(false, false)
+	   	doc.FillMeta(false, false)
 
-	if appErr := doc.Update(bson.D{{Key: "_id", Value: exists.ID}}, exists, "appKey"); appErr != nil {
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-		return
-	}
+	   	if appErr := doc.Update(bson.D{{Key: "_id", Value: exists.ID}}, exists, "appKey"); appErr != nil {
+	   		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+	   		return
+	   	} */
 
 	c.Status(http.StatusOK)
 }
 
 func AppAppsDelete(c *gin.Context) {
 
-	doc := models.NewAppAppModel(c)
+	/* 	doc := models.NewAppAppModel(c)
 
-	id := doc.GetValues().Query.ID
-	if id == nil {
-		appErr := service_log.Error(0, http.StatusBadRequest, "CONTROLLER::AppApps::Update:", "", "invalid id")
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-		return
-	}
+	   	id := doc.GetValues().Query.ID
+	   	if id == nil {
+	   		appErr := service_log.Error(0, http.StatusBadRequest, "CONTROLLER::AppApps::Update:", "", "invalid id")
+	   		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+	   		return
+	   	}
 
-	if appErr := doc.Delete(*id); appErr != nil {
-		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
-		return
-	}
+	   	if appErr := doc.Delete(*id); appErr != nil {
+	   		c.AbortWithStatusJSON(appErr.HttpCode, appErr)
+	   		return
+	   	} */
 
 	c.Status(http.StatusOK)
 }
